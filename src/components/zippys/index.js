5376fee11c70d3b3a94addd4beba7de2e7582e99
loreleim <lorelei.miyamura@gmail.com>
2021-02-27 13:53:15 -0800

    filesaver + jszip

--- src/components/zippys/index.js
+++ src/components/zippys/index.js
@@ -1,9 +1,22 @@
 import style from "./index.module.scss";
+import * as JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function Zippys() {
+
+  function generateZip() {
+    var zip = new JSZip();
+    zip.file("Hello.txt", "Hello World\n");
+    zip.file("Hello.txt", "Hello World\n");
+    zip.generateAsync({type:"blob"})
+    .then((content) => {
+    saveAs(content, "example.zip");
+});
+  }
   return (
     <div className={style.zippysContainer}>
       <h1>Zip It!</h1>
+      <button onClick={()=> generateZip()}>Zip It</button>
     </div>
   );
 }
